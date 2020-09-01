a42c0946f3621de58e26c0f4275f8ec87dae0204
oscar ortiz <ortizg.oscar@gmail.com>
2020-09-01 07:45:45 -0300

    work in progress

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,7 +1,8 @@
 import { connect } from 'react-redux'
-import { toggleTodo } from '../actions'
+// import { toggleTodo } from '../actions'
+import { toggleTodo} from '../features/todos/todosSlice';
 import TodoList from '../components/TodoList'
-import { VisibilityFilters } from '../actions'
+import { VisibilityFilters } from '../features/filters/filtersSlice' ;
 
 const getVisibleTodos = (todos, filter) => {
   switch (filter) {
@@ -20,9 +21,11 @@ const mapStateToProps = state => ({
   todos: getVisibleTodos(state.todos, state.visibilityFilter)
 })
 
-const mapDispatchToProps = dispatch => ({
-  toggleTodo: id => dispatch(toggleTodo(id))
-})
+// const mapDispatchToProps = dispatch => ({
+//   toggleTodo: id => dispatch(toggleTodo(id))
+// })
+
+const mapDispatchToProps = { toggleTodo};
 
 export default connect(
   mapStateToProps,
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux'
-import todos from './todos'
-import visibilityFilter from './visibilityFilter'
+// import todos from './todos'
+import todosReducer from '../features/todos/todosSlice';
+import visibilityFilterReducer from '../features/filters/filtersSlice'
 
 export default combineReducers({
-  todos,
-  visibilityFilter
+  todos: todosReducer,
+  visibilityFilter: visibilityFilterReducer
 })
